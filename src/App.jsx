ef15790698c723d212021f61be8fdda7f74476d1
sunita0987 <sunita0987546@gmail.com>
2025-07-08 10:25:11 +0530

    project-setup

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import React from "react";
+import NavBar from "./components/NavBar";
+
+function App() {
+  return (
+    <>
+      <NavBar />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NavBar() {
+  return (
+    <div>
+      <h1 className='text-6xl font-bold text-red-400'>hello world  </h1>
+    </div>
+  )
+}
+
+export default NavBar
